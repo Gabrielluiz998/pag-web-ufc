78c5911c95396857199771c61149a2c7838bcac1
Gabrielluiz998 <gllf1@aluno.ifal.edu.br>
2024-12-01 15:36:52 -0300

    tela de inicio concluida

--- SCRIPTS/Navegações.js
+++
@@ -1,3 +0,0 @@
-function abrirTelaEventos() {
-    window.location.href = "telaEventos.html"
-}
---
+++ SCRIPTS/TelaInicial.js
@@ -0,0 +1,21 @@
+function abrirTelaEventos() {
+    window.location.href = "telaEventos.html"
+}
+
+
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+    
+});
+
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach( (element) => myObserver.observe(element))
